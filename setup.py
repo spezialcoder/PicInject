d7fb8e70b5e325683d8bc1e1f7ddac88dfe7195f
Lewin Sorg <developermind405@gmail.com>
2020-06-06 01:58:38 +0200

    Add files via upload

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from setuptools import setup,find_packages
 
 setup(
     name="picinject",
-    version="1.0",
+    version="0.1",
     description="With picinject you can simply hide files in pictures and extract them again when you need them",
     install_requires=["colorama"],
     python_requires=">=3.6.*",
     author="Lewin Sorg",
     author_email="developermind405@gmail.com",
     scripts=["src/picinject.py"],
-    url="https://github.com/spezialcoder/PicInject"
+    url="https://github.com/spezialcoder/PicInject",
+    license="MIT"
 )
